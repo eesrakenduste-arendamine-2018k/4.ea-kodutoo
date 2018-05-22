d17451129ac9ccb5c9273d05e8f9e4de12ed6f32
Toolsten <Toolsten@users.noreply.github.com>
2018-05-22 14:13:56 +0300

    proovisin leiutada midagi

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-bodyWeightArray = []
-labelsArray = [];
-dataArray = [];
-
-
 bodyWeightToArray = function(){
     bodyWeight = document.getElementById('bodyWeight').value
     bodyWeightDate = getDate()
     bodyWeightArray.push(bodyWeight+";"+bodyWeightDate)
+    localStorage.setItem("bodyArray", JSON.stringify(bodyWeightArray));
     splitArrays()
     //Storage.prototype.setObject = function(key, value) {
     //this.setItem(key, JSON.stringify(value));
@@ -23,52 +19,66 @@ getDate = function(){
     newdate = day + "/" + month + "/" + year;
     return newdate
 }
+
 splitArrays = function(){
-   
+bodyWeightArray = JSON.parse(localStorage.getItem("bodyArray"));
+console.log(bodyWeightArray)
 bodyWeightArray.forEach(function(element){
-    var arrayElement = element.split(';');
+    arrayElement = element.split(';');
     dataArray.push(arrayElement[0]);
     labelsArray.push(arrayElement[1]);
-    slicedLabel = labelsArray.slice(-1)[0]
-    console.log(slicedLabel)
-    slicedData = dataArray.slice(-1)[0]
-    console.log(slicedData)
-    
-    console.log("pushitud")
 })}
 
-var ctx = document.getElementById('line-chart');
-var config = {
+readOldData = function(){
+bodyWeightArray = JSON.parse(localStorage.getItem("bodyArray"));
+console.log(bodyWeightArray)
+bodyWeightArray.forEach(function(element){
+    arrayElement = element.split(';');
+    dataArray.push(arrayElement[0]);
+    labelsArray.push(arrayElement[1]);
+})
+
+//console.log(JSON.parse(localStorage.getItem("bodyArray")))
+//console.log(dataArray)
+//console.log(labelsArray)
+/*document.getElementById('addData').addEventListener('click', function() {
+    //siin on probleem, ta mitte ei uuenda vaid ta liidab kaks massiivi üksteise otsa
+    
+    //createNewChart.config.data.datasets = {data: [dataArray]}
+    console.log(config)
+    createNewChart.destroy(config)
+    console.log(config)
+    createNewChart.update(config)
+    
+})
+*/
+ctx = document.getElementById('line-chart');
+
+config = {
     // The type of chart we want to create
     type: 'line',
 
     // The data for our dataset
     data: {
-        labels: ["kuupäev"],
+        labels: labelsArray,
         datasets: [{
-            label: "My First dataset",
             fill: false,
             backgroundColor: 'rgb(51, 153, 255)',
             borderColor: 'rgb(51, 153, 255)',
-            data: [90, 100],
+            data: dataArray,
         }]
     },
 
     // Configuration options go here
     options: {}
 };
-console.log(dataArray)
-console.log(labelsArray)
+}
 document.getElementById('addData').addEventListener('click', function() {
-			if (config.data.datasets.length > 0) {
-                console.log(slicedLabel, "newLabel")
-				config.data.labels.push(slicedLabel);
-				config.data.datasets.forEach(function(dataset) {
-                    console.log(slicedData, "newData")
-					dataset.data.push(slicedData);
-				});
+    //siin on probleem, ta mitte ei uuenda vaid ta liidab kaks massiivi üksteise otsa
+    //liidab sellele dataarrayle uuesti selle dataarray otsa
+    //mdea pop töötab
+    
+    createNewChart.update(config)
+    //createNewChart.destroy()
 
-				createNewChart.update();
-			}
-		});
-var createNewChart = new Chart(ctx, config)
+})
