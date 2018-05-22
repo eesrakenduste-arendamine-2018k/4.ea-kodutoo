e53b7f40f623eb45011d3777f7c274f08fc10b16
Erik Enden <erikenden@gmail.com>
2018-05-22 11:49:04 +0300

    Added serviceworker

---
+++ serviceWorker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'js_pong'
+const cachedUrls = [
+  'index.html',
+  'game.js',
+  'pong.js',
+  'pong.css'
+]
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('cache opened')
+        return cache.addAll(cachedUrls)
+      })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') { }
+  event.respondWith(
+    caches
+      .match
+      .then(function (cached) {
+        const networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve)
+        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url)
+        return cached || networked
+
+        function fetchedFromNetwork (response) {
+          const cacheCopy = response.clone()
+          console.log('WORKER: fetch response from network', event.request.url)
+          caches
+            .open(version + 'pages')
+            .then(function add (cache) {
+              cache.put(event.request, cacheCopy)
+            })
+            .then(function () {
+              console.log('WORKER: fetch response stored in cache.', event.request.url)
+            })
+          return response
+        }
+        function unableToResolve () {
+          console.log('WORKER: fetch request failed in both cache and network.', event.request.url)
+          return new Response('<h1>Service unavailable</h1>', {
+            status: 503,
+            statusText: 'Service unavailable',
+            headers: new Headers({
+              'Content-Type': 'text-html'
+            })
+          })
+        }
+      })
+  )
+})
