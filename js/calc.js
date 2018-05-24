72877138590c47a65ea114abe433c70baa50722a
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-05-24 11:50:23 +0300

    Robusting calculator
    allowing values which are greater than 0

--- js/calc.js
+++ js/calc.js
@@ -4,9 +4,13 @@ function clicked(){
 	const height = document.getElementById('vertical').value;
 	const inch = document.getElementById('diagonal').value;
 	let answer = '';
-	answer = precisionRound((Math.sqrt((width*width)+(height*height))/inch),2);
+	if(width>0 && height>0 && inch>0){
+		answer = precisionRound((Math.sqrt((width*width)+(height*height))/inch),2);	
+	}
+	else{
+		answer = 'Values must be greater than 0';
+	}
 	document.getElementById('result').innerHTML = answer;
-	
 	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
 	function precisionRound(number, precision) {
 		var factor = Math.pow(10, precision);
