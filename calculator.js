d15d299b8e7ab0c442c4999a442b9b98277758b1
mkivikin <mkivikin@users.noreply.github.com>
2018-05-24 23:40:38 +0300

    cleanup

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ let CAD = 1.5093263;
 let JPY = 130.74914;
 let MXN = 23.2767315;
 
-$(function () {
+/*$(function () {
 	$(".content").click(function () {
 
 		let value = $(this).find(".number").text();
@@ -16,7 +16,6 @@ $(function () {
 				let a = $(this).text();
 				if (!a) {
 					$(this).text(value);
-					$(this).addClass("nocircle");
 					return false;
 				}
 			});
@@ -25,31 +24,45 @@ $(function () {
 				let a = $(this).text();
 				if (a) {
 					$(this).text("");
-					$(this).removeClass("nocircle");
 					return false;
 				}
 			});
 		}
 	});
-});
+});*/
 
-function init(){
-	EUR = document.getElementById("EUR");
-    USD = document.getElementById("USD");
-    GBP = document.getElementById("GBP");
-    RUB = document.getElementById("RUB");
-    CAD = document.getElementById("CAD");
-    JPY = document.getElementById("JPY");
-    MXN = document.getElementById("MXN");
-}
-
-function converter(){
+/*function init(){
+		EUR = document.getElementById("EUR").value;
+    USD = document.getElementById("USD").value;
+    GBP = document.getElementById("GBP").value;
+    RUB = document.getElementById("RUB").value;
+    CAD = document.getElementById("CAD").value;
+    JPY = document.getElementById("JPY").value;
+    MXN = document.getElementById("MXN").value;
+}*/
+function eurConverter(){
+	EUR = parseFloat(document.getElementById("EUR").value);
+	/*console.log(EUR);
+	let NEWUSD = parseFloat(EUR.value) * parseFloat(USD);
+	console.log(USD);
+	document.getElementById("USD").value = parseFloat(NEWUSD);*/
 	USD.value = parseFloat(EUR.value) * USD;
-	GBP.value = parseFloat(EUR.value) * GBP;
-	RUB.value = parseFloat(EUR.value) * RUB;
-	CAD.value = parseFloat(EUR.value) * CAD;
-	JPY.value = parseFloat(EUR.value) * JPY;
-	MXN.value = parseFloat(EUR.value) * MXN;
+	document.getElementById("USD").value = USD.value;
 }
+/*function converter(){
+	EUR.value = document.getElementById("EUR").value;
+	USD.value = parseFloat(EUR.value) * USD;
+	document.getElementById("USD").value = USD
+	GBP.value = parseFloat(EUR.value) * GBP;
+	document.getElementById("GBP").value = GBP
+	RUB.value = parseFloat(EUR.value) * RUB;
+	document.getElementById("RUB").value = RUB
+	CAD.value = parseFloat(EUR.value) * CAD;
+	document.getElementById("CAD").value = CAD
+	JPY.value = parseFloat(EUR.value) * JPY;
+	document.getElementById("JPY").value = JPY
+	MXN.value = parseFloat(EUR.value) * MXN;
+	document.getElementById("MXN").value = MXN
+}*/
 
-init();
+//init();
