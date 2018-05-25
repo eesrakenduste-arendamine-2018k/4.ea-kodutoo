b16cd6484a7650685f983cd6f4dba582b8725b58
mkivikin <mkivikin@users.noreply.github.com>
2018-05-25 03:00:44 +0300

    Gets values from keypad, still allows to enter more than one .

--- keypad.js
+++ keypad.js
@@ -1,4 +1,26 @@
-$("button").click(function() {
-    var clicked = $(this).val();
-    alert(clicked);
+let string = "";
+
+$(document).ready(function() {
+  $("button").click(function() {
+      var clicked = $(this).val();
+      if(clicked == "del") {
+        if (string.length > 0) {
+          string.slice(0, -1);
+        } else {
+          console.log("Nothing to delete");
+        }
+      }
+      if(clicked == ".") {
+        if(!string.includes(".")){
+          string = string+clicked;
+        } else {
+          console.log(". Already exists");
+        }
+      }
+      if(0 <= clicked <= 9 && clicked !== "del") {
+        string = string + clicked;
+      }
+      console.log(clicked);
+      console.log(string);
+  });
 });
