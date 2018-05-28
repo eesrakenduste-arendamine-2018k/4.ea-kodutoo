7b2c8915a36af12c8f2835269620343cd340eb0a
ciruss <johanlaas4@gmail.com>
2018-05-29 00:10:43 +0300

    favicons and style changes

--- calc.js
+++ calc.js
@@ -5,15 +5,15 @@ const MINCALORIES = document.getElementById('minCalories')
 const ACTIVECALORIES = document.getElementById('activeCalories')
 const ACTIVITYLEVEL = document.getElementById('activityLevel')
 const BODYMASSINDEX = document.getElementById('bmiIndex')
-let activityLevelIndex, result, bmiIndex
+let activityLevelIndex, bmiIndex
 let activityMultiplier = 1.2
 
 const maleDailyCalories = () => {
-  return result = 88.362 + (13.397 * WEIGHT.value) + (4.799 * HEIGHT.value) - (5.677 * AGE.value)
+  return 88.362 + (13.397 * WEIGHT.value) + (4.799 * HEIGHT.value) - (5.677 * AGE.value)
 }
 
 const femaleDailyCalories = () => {
-  return result = 447.593 + (9.247 * WEIGHT.value) + (3.098 * HEIGHT.value) - (4.330 * AGE.value)
+  return 447.593 + (9.247 * WEIGHT.value) + (3.098 * HEIGHT.value) - (4.330 * AGE.value)
 }
 
 const dailyMinCalories = () => {
@@ -33,6 +33,6 @@ ACTIVITYLEVEL.addEventListener('change', () => {
 document.getElementById('calculate').addEventListener('click', (event) => {
   event.preventDefault()
   // BODYMASSINDEX.innerHTML = `${Math.round(bodyMassIndex() * 100) / 100 } on teie kehamassiindeks`
-  MINCALORIES.innerHTML = `${dailyMinCalories()}cal. See on sinu päevane minimaalne kalori vajadus.`
-  ACTIVECALORIES.innerHTML = `${Math.round(dailyMinCalories() * activityMultiplier)}cal. See on sinu päevane kalori vajadus kaalu säilitamiseks.`
+  MINCALORIES.innerHTML = `<b>${dailyMinCalories()}cal.</b> See on sinu päevane minimaalne kalori vajadus.`
+  ACTIVECALORIES.innerHTML = `<b>${Math.round(dailyMinCalories() * activityMultiplier)}cal.</b> See on sinu päevane kalori vajadus kaalu säilitamiseks.`
 })
