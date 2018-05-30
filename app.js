9f827225e458dbb41c045f6599d577d0c1d40310
Mihkel Haava <mhaava@tlu.ee>
2018-05-30 21:10:03 +0300

    algus

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-function startGame() {
+ function shake(event) {
+                  const xGravity = event.accelerationIncludingGravity.x
+                    document.getElementById("testObj").innerHTML = xGravity
+                  
+                  if (xGravity > 10) {
+                    
+                    //navigator.vibrate(300)
+                    document.getElementById("testObj").innerHTML = "works";
+                  
+                  }
+                
+            }
+            window.addEventListener('devicemotion', shake.bind(this))
 
+
+function startGame() {
+	document.getElementById("play").style.display = "none";
+	document.getElementById("readme").style.display = "none";
 }
