85f88fe3e3550be103009efbd7ec474c429f6d31
Andre Martov <andremartov@github.com>
2018-05-30 18:52:40 +0300

    Natukene tehtud

---
+++ todo.js
@@ -0,0 +1,29 @@
+// Close nupp to do listi lisatu kinni panemiseks
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Peidad itemi to do listis 2ra
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Kui vajutad lisatud ylesandele, ilmub kiri checked
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
