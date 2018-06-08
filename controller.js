9a6f74622b927a86ebe51c760876b3b5c450430a
Mihkel Mägi <mihkel26@tlu.ee>
2018-06-08 13:55:28 +0300

    Revert "Merge pull request #1 from mihkel26/sten"
    This reverts commit bcc0523880e3439026727efbaa89a83a06950e09, reversing
    changes made to a332a35a063e1c77850ba81056face8a6aad40f6.

--- controller.js
+++ controller.js
@@ -1,31 +1,29 @@
 let MOUSE_LEFT = 1,
-    MOUSE_RIGHT = 3;
+  MOUSE_RIGHT = 3;
 let clicks = 0;
 
 
-canvas.addEventListener('mousedown', function (e) {
-    let x = e.clientX - canvas.offsetLeft,
-        y = e.clientY - canvas.offsetTop;
+canvas.addEventListener('mousedown', function(e) {
+  let x = e.clientX - canvas.offsetLeft,
+    y = e.clientY - canvas.offsetTop;
 
-    // hit test
-    let modelCoordinates = viewToModel(x, y);
+  // hit test
+  let modelCoordinates = viewToModel(x, y);
 
-    if (e.which === MOUSE_LEFT) {
-        clicks += 1;
-        //console.log(clicks);
-        openBlock(modelCoordinates.x, modelCoordinates.y)
-    } else if (e.which === MOUSE_RIGHT) {
-        flagBlock(modelCoordinates.x, modelCoordinates.y)
-    }
+  if (e.which === MOUSE_LEFT) {
+    clicks += 1;
+    //console.log(clicks);
+    openBlock(modelCoordinates.x, modelCoordinates.y)
+  } else if (e.which === MOUSE_RIGHT) {flagBlock(modelCoordinates.x, modelCoordinates.y)}
 
-    render();
+  render();
 
-    return false;
+  return false;
 });
 
-canvas.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
-    return false;
+canvas.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+  return false;
 }, false);
 
 //code from: https://www.youtube.com/watch?v=LRnnNInjmN0
--- minesweeper.js
+++ minesweeper.js
@@ -1,168 +1,143 @@
+
+
 let COLS = 10, ROWS = 10, MINES = 10;
 let board = [];
 let state = [];
 let STATE_CLOSED = 0,
-    STATE_FLAGGED = 1,
-    STATE_OPENED = 2;
+  STATE_FLAGGED = 1,
+  STATE_OPENED = 2;
 let BLOCK_MINE = -1;
 let playing = true;
 let blocksOpened = 0;
-let startDate;
-let endDate;
-let startTime;
-let endTime;
-let timer;
+
 
 
 function inBounds(x, y) {
-    return x >= 0 && y >= 0
-        && x < COLS && y < ROWS;
+  return x >= 0 && y >= 0
+    && x < COLS && y < ROWS;
 }
 
 function countMinesAround(x, y) {
-    let count = 0;
-    for (let dx = -1; dx <= 1; ++dx) {
-        for (let dy = -1; dy <= 1; ++dy) {
-            if (dx == 0 && dy == 0) {
-                continue;
-            }
-            let yy = y + dy,
-                xx = x + dx;
-            if (inBounds(xx, yy)) {
-                if (board[yy][xx] == BLOCK_MINE) {
-                    ++count;
-                }
-            }
+  let count = 0;
+  for (let dx = -1; dx <= 1; ++dx) {
+    for (let dy = -1; dy <= 1; ++dy) {
+      if (dx == 0 && dy == 0) {
+        continue;
+      }
+      let yy = y + dy,
+        xx = x + dx;
+      if (inBounds(xx, yy)) {
+        if (board[yy][xx] == BLOCK_MINE) {
+          ++count;
         }
+      }
     }
-    return count;
+  }
+  return count;
 }
 
 function init() {
-    startDate = new Date();
-    startTime = startDate.getTime() / 1000;
 
-    //this.registerServiceWorker()
+  //this.registerServiceWorker()
 
-    for (let y = 0; y < ROWS; ++y) {
-        board.push([]);
-        state.push([]);
-        for (let x = 0; x < COLS; ++x) {
-            board[y].push(0);
-            state[y].push(STATE_CLOSED);
-        }
+  for (let y = 0; y < ROWS; ++y) {
+    board.push([]);
+    state.push([]);
+    for (let x = 0; x < COLS; ++x) {
+      board[y].push(0);
+      state[y].push(STATE_CLOSED);
     }
+  }
 
-    for (let mine = 0; mine < MINES; ++mine) {
-        let x, y;
-        do {
-            x = Math.floor(Math.random() * COLS),
-                y = Math.floor(Math.random() * ROWS);
-        } while (board[y][x] == BLOCK_MINE);
+  for (let mine = 0; mine < MINES; ++mine) {
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * COLS),
+        y = Math.floor(Math.random() * ROWS);
+    } while (board[y][x] == BLOCK_MINE);
 
-        board[y][x] = BLOCK_MINE;
+    board[y][x] = BLOCK_MINE;
+  }
+
+  for (let y = 0; y < ROWS; ++y) {
+    for (let x = 0; x < COLS; ++x) {
+      if (board[y][x] != BLOCK_MINE) {
+        board[y][x] = countMinesAround(x, y);
+      }
     }
-
-    for (let y = 0; y < ROWS; ++y) {
-        for (let x = 0; x < COLS; ++x) {
-            if (board[y][x] != BLOCK_MINE) {
-                board[y][x] = countMinesAround(x, y);
-            }
-        }
-    }
+  }
 
 }
 
 function openBlock(x, y) {
-    blocksOpened += 1;
+  blocksOpened += 1;
 
-    if (!playing) {
-        return;
-    }
-    if (state[y][x] == STATE_FLAGGED) {
-        return;
-    }
+  if (!playing) {
+    return;
+  }
+  if (state[y][x] == STATE_FLAGGED) {
+    return;
+  }
 
-    if (board[y][x] == BLOCK_MINE) {
-        alert('Sa kaotasid!');
-        playing = false;
-        revealBoard(false);
-        return;
-    }
+  if (board[y][x] == BLOCK_MINE) {
+    alert('Sa kaotasid!');
+    playing = false;
+    revealBoard(false);
+    return;
+  }
 
-    state[y][x] = STATE_OPENED;
-    if (board[y][x] == 0) {
-        for (let dx = -1; dx <= 1; ++dx) {
-            for (let dy = -1; dy <= 1; ++dy) {
-                let xx = x + dx,
-                    yy = y + dy;
-                if (inBounds(xx, yy)) {
-                    if (state[yy][xx] != STATE_OPENED) {
-                        openBlock(xx, yy);
-                    }
-                }
-            }
+  state[y][x] = STATE_OPENED;
+  if (board[y][x] == 0) {
+    for (let dx = -1; dx <= 1; ++dx) {
+      for (let dy = -1; dy <= 1; ++dy) {
+        let xx = x + dx,
+          yy = y + dy;
+        if (inBounds(xx, yy)) {
+          if (state[yy][xx] != STATE_OPENED) {
+            openBlock(xx, yy);
+          }
         }
+      }
     }
+  }
 
-    if (checkVictory()) {
-        alert('Sa said hakkama!');
-        playing = false;
-        revealBoard(true);
-        endDate = new Date();
-        endTime = endDate.getTime() / 1000;
-        timer = endTime - startTime;
-        console.log(timer);
-        if (localStorage.getItem("clicks") === null){
-            console.log("value in localStorage not found")
-            localStorage.setItem("clicks", JSON.stringify(clicks))
-            localStorage.setItem("time", JSON.stringify(timer))
-
-        }
-        if (localStorage.getItem("clicks") !== null){
-            console.log("value in localStorage found")
-            let localStorageTime = JSON.parse(localStorage.getItem("time"))
-            if (localStorageTime > timer ) {
-                localStorage.setItem("clicks", JSON.stringify(clicks))
-                localStorage.setItem("time", JSON.stringify(timer))
-            }
-            else {
-                console.log("better score found in localStorage")
-            }
-        }
-    }
+  if (checkVictory()) {
+    alert('Sa said hakkama!');
+    playing = false;
+    revealBoard(true);
+  }
 }
 
 function checkVictory() {
-    for (let y = 0; y < ROWS; ++y) {
-        for (let x = 0; x < COLS; ++x) {
-            if (board[y][x] != BLOCK_MINE) {
-                if (state[y][x] != STATE_OPENED) {
-                    return false;
-                }
-            }
+  for (let y = 0; y < ROWS; ++y) {
+    for (let x = 0; x < COLS; ++x) {
+      if (board[y][x] != BLOCK_MINE) {
+        if (state[y][x] != STATE_OPENED) {
+          return false;
         }
+      }
     }
-    return true;
+  }
+  return true;
 }
 
 function flagBlock(x, y) {
-    if (state[y][x] == STATE_OPENED) {
-        return;
-    }
-    state[y][x] = 1 - state[y][x];
+  if (state[y][x] == STATE_OPENED) {
+    return;
+  }
+  state[y][x] = 1 - state[y][x];
 }
 
 function revealBoard(victorious) {
-    for (let y = 0; y < ROWS; ++y) {
-        for (let x = 0; x < COLS; ++x) {
-            if (board[y][x] == BLOCK_MINE && victorious) {
-                state[y][x] = STATE_FLAGGED;
-                continue;
-            }
-            state[y][x] = STATE_OPENED;
-        }
+  for (let y = 0; y < ROWS; ++y) {
+    for (let x = 0; x < COLS; ++x) {
+      if (board[y][x] == BLOCK_MINE && victorious) {
+        state[y][x] = STATE_FLAGGED;
+        continue;
+      }
+      state[y][x] = STATE_OPENED;
     }
+  }
 }
 
 init()
