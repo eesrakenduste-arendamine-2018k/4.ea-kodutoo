59cb8449f6b29fe39e840611edd0f74290f1eaf5
Mihkel Mägi <mihkel26@tlu.ee>
2018-06-09 14:50:04 +0300

    sw fix

--- sw.js
+++ sw.js
@@ -7,6 +7,10 @@ let urlsToCache = [
     '/4.ea-kodutoo/minesweeper.js',
     '/4.ea-kodutoo/render.js',
     '/4.ea-kodutoo/style.css',
+    '/4.ea-kodutoo/images/',
+    '/4.ea-kodutoo/images/bg.jpg',
+    '/4.ea-kodutoo/images/bombb.svg',
+    '/4.ea-kodutoo/images/flag.svg',
     '/4.ea-kodutoo/sw.js'
 ];
 
