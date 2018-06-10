febc1758378e6eb776f549f8881f3683927421da
Sten Markus Laht <31847424+stenmarkus@users.noreply.github.com>
2018-06-10 19:25:49 +0300

    fixed indentation

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 document.getElementById("newGame").style.display="none";
-	this.init()
+this.init()
 
 window.onload = function firstCard(){
 	var card = "kaarditagune.png";
 	var displayBack = "<img src='" + card + "' width='186' height='270'/>";
 	document.getElementById("result").innerHTML = displayBack;
-	}
+}
 function displayImage(){
 	var pildid = ["images/artu2.png", "images/artu3.png", "images/artu4.png", "images/artu5.png", "images/artu6.png", "images/artu7.png", "images/artu8.png", "images/artu9.png", "images/artu10.png", "images/artupoiss.png", "images/artuemand.png", "images/artukunn.png", "images/artuass.png", "images/poti2.png", "images/poti3.png", "images/poti4.png", "images/poti5.png", "images/poti6.png", "images/poti7.png", "images/poti8.png", "images/poti9.png", "images/poti10.png", "images/potipoiss.png", "images/potiemand.png", "images/potikunn.png", "images/potiass.png", "images/ruutu2.png", "images/ruutu3.png", "images/ruutu4.png", "images/ruutu5.png", "images/ruutu6.png", "images/ruutu7.png", "images/ruutu8.png", "images/ruutu9.png", "images/ruutu10.png", "images/ruutupoiss.png", "images/ruutuemand.png", "images/ruutukunn.png", "images/ruutuass.png", "images/risti2.png", "images/risti3.png", "images/risti4.png", "images/risti5.png", "images/risti6.png", "images/risti7.png", "images/risti8.png", "images/risti9.png", "images/risti10.png", "images/ristipoiss.png", "images/ristiemand.png", "images/ristikunn.png", "images/ristiass.png"];
 
@@ -13,12 +13,12 @@ function displayImage(){
 	var displayPic = "<img src='" + item + "' width='186' height='270'/>";
 	document.getElementById("result").innerHTML = displayPic;
 
-	}
-	var score1 = 26
+}
+var score1 = 26
 	
-	document.getElementById("addPic").addEventListener("click", addCard);
+document.getElementById("addPic").addEventListener("click", addCard);
 
-	function addCard() {
+function addCard() {
     document.getElementById("score").innerHTML = score1 += 1;
 	if (score1 == 52){
 		document.getElementById("score").innerHTML = "Mäng on läbi! Sa võitsid";
@@ -29,10 +29,10 @@ function displayImage(){
 		document.getElementById("result").style.display="none";
 	}
 }
-	document.getElementById("removePic").addEventListener("click", removeCard);
+document.getElementById("removePic").addEventListener("click", removeCard);
 
-	function removeCard() {
-    document.getElementById("score").innerHTML = score1 -= 1;
+function removeCard() {
+	document.getElementById("score").innerHTML = score1 -= 1;
 	if (score1 == 0){
 		document.getElementById("score").innerHTML = "Mäng on läbi! Sa kaotasid";
 		document.getElementById("addPic").style.display = "none";
@@ -40,7 +40,7 @@ function displayImage(){
 		document.getElementById("newPic").style.display = "none";
 		document.getElementById("newGame").style.display="inline";
 		document.getElementById("result").style.display="none";
-}
+	}
 	
 }
 function startNewGame(){
@@ -48,7 +48,8 @@ function startNewGame(){
 }
 function init() {
 	window.addEventListener('devicemotion', this.triggerMotion.bind(this))
-	}
+}
+
 function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
@@ -57,16 +58,17 @@ function sleep(milliseconds) {
     }
   }
 }
+
 function triggerMotion (event) {
-      const xGravity = event.accelerationIncludingGravity.x
-	  const test = true;
-	  
-      if ((xGravity + xGravity + xGravity)/3 > 10) {
-        this.displayImage()
-		sleep(500)
+    const xGravity = event.accelerationIncludingGravity.x
+	const test = true;
+    if ((xGravity + xGravity + xGravity)/3 > 10) {
+    this.displayImage()
+	sleep(500)
 		
-	  }
-    }
+	}
+}
+	this.registerServiceWorker()
 
 	
 	//Register service worker
@@ -84,4 +86,4 @@ function triggerMotion (event) {
 		}
     }
 	
-	registerServiceWorker();
+	
